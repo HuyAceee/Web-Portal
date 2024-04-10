b328f49646318d6f292e902a16b30ee4c2af9278
HuyDV <dovanhuyabcd@gmail.com>
2024-04-10 17:55:15 +0700

    update code semester

--- src/sections/semester/view/SemesterView.tsx
+++ src/sections/semester/view/SemesterView.tsx
@@ -1,13 +1,32 @@
-import { Stack, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import Iconify from "components/Iconify";
+import { NEW_SEMESTER } from "constant/router";
 import { useTranslation } from "react-i18next";
+import { RouterLink } from "routes/components";
 
 // ----------------------------------------------------------------------
 
 export default function SemesterView() {
   const { t } = useTranslation();
   return (
-    <Stack spacing={3}>
-      <Typography variant="h4">{t("semester.list.title")}</Typography>
-    </Stack>
+    <Container>
+      <Stack
+        spacing={3}
+        direction="row"
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Typography variant="h4">{t("semester.list.title")}</Typography>
+        <RouterLink href={NEW_SEMESTER}>
+          <Button
+            variant="contained"
+            color="inherit"
+            startIcon={<Iconify icon="eva:plus-fill" />}
+          >
+            {t("notification.button.newNotification")}
+          </Button>
+        </RouterLink>
+      </Stack>
+    </Container>
   );
 }
